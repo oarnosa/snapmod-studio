2f4bd20bc207fa89c262913cd46fe00ac58cd1e7
Orlando Arnosa <oarnosa@gmail.com>
2024-06-11 20:45:05 -0400

    Setup desktop nav

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,16 +1,7 @@
-import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
-import React from 'react';
-
 const Home = () => {
   return (
     <div>
       <p>Home</p>
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
     </div>
   );
 };
