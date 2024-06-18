575305d5f46b01285783490937fba43d6ddeba96
Orlando Arnosa <oarnosa@gmail.com>
2024-06-18 12:11:34 -0400

    Integrate with Cloudinary AI

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -13,6 +13,7 @@ import { useState, useTransition } from 'react';
 import { Button } from '../ui/button';
 import { Input } from '../ui/input';
 import { CustomField } from './CustomField';
+import MediaUploader from './MediaUploader';
 
 export const formSchema = z.object({
   title: z.string(),
@@ -167,6 +168,23 @@ const TransformationForm = ({
             )}
           </div>
         )}
+
+        <div className="media-uploader-field">
+          <CustomField
+            control={form.control}
+            name="publicId"
+            className="flex size-full flex-col"
+            render={({ field }) => (
+              <MediaUploader
+                onValueChange={field.onChange}
+                setImage={setImage}
+                publicId={field.value}
+                image={image}
+                type={type}
+              />
+            )}
+          />
+        </div>
 
         <div className="flex flex-col gap-4">
           <Button
